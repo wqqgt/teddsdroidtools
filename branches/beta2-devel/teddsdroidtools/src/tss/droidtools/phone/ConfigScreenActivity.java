2527e94b490263947996403f29751e1c14c182f3
teddsdroidtools <teddsdroidtools@353b4ea0-66fb-f038-3624-99da76164efb>
2010-05-10 06:15:10 +0000

    added silence ringer call when answering/ignoring calls added call answer and screen guard configurations all features now enabled by default updated config screen

--- branches/beta2-devel/teddsdroidtools/src/tss/droidtools/phone/ConfigScreenActivity.java
+++ branches/beta2-devel/teddsdroidtools/src/tss/droidtools/phone/ConfigScreenActivity.java
@@ -20,11 +20,13 @@ public class ConfigScreenActivity extends BaseActivity {
 
         setContentView(R.layout.main);
 
-        renderConfigCheckbox(Hc.PREF_PHONE_TOOLS_KEY,false,R.id.phoneToolsCheckBox,"Phone Tools");
+        renderConfigCheckbox(Hc.PREF_PHONE_TOOLS_KEY,true,R.id.phoneToolsCheckBox,"ALL Phone Tools have been");
+        renderConfigCheckbox(Hc.PREF_CALL_ANSWER_TOOLS_KEY,true,R.id.callAnswerTools,"Call Answer Tools");
         renderConfigCheckbox(Hc.PREF_ANSWER_WITH_CAMERA_KEY,true,R.id.cameraAnswerCheckBox,"Camera Button Answer");
         renderConfigCheckbox(Hc.PREF_ANSWER_WITH_TRACKBALL_KEY,true,R.id.trackballAnswerCheckBox,"Trackball Answer");
-        renderConfigCheckbox(Hc.PREF_ANSWER_WITH_BUTTON_KEY,false,R.id.touchscreenButtonAnswerCheckBox,"Touchscreen Button Answer");
-        renderConfigCheckbox(Hc.PREF_ALLOW_REJECT_KEY,false,R.id.rejectCallsCheckBox,"Reject Call Feature");
+        renderConfigCheckbox(Hc.PREF_ANSWER_WITH_BUTTON_KEY,true,R.id.touchscreenButtonAnswerCheckBox,"Touchscreen Button Answer");
+        renderConfigCheckbox(Hc.PREF_ALLOW_REJECT_KEY,true,R.id.rejectCallsCheckBox,"Reject Call Feature");
+        renderConfigCheckbox(Hc.PREF_SCREEN_GUARD_TOOLS_KEY,true,R.id.inCallScreenGuardCheckBox,"In-Call Screen Guard");
         renderConfigCheckbox(Hc.PREF_DEBUG_LOGGING_KEY,true,R.id.debugLoggingCheckBox,"Debug Logging");
         
     }
